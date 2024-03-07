155db80803066d3e50338743ab2c61f3252f57d8
thinmoto <thinmoto@gmail.com>
2024-03-07 11:02:40 +0100

    ~ fix

--- packages/admin/src/Http/Livewire/Components/FieldTypes/FileFieldtype.php
+++ packages/admin/src/Http/Livewire/Components/FieldTypes/FileFieldtype.php
@@ -106,9 +106,9 @@ class FileFieldtype extends Component
 		if($this->searchTerm)
 			return Asset::query()->whereRelation('file', function($query){
 				$query->where('file_name', 'LIKE', '%'.$this->searchTerm.'%');
-			})->paginate(16, ['*'], 'assetsPage');
+			})->paginate(10, ['*'], 'assetsPage');
 
-        return Asset::paginate(16, ['*'], 'assetsPage');
+        return Asset::paginate(10, ['*'], 'assetsPage');
     }
 
     /**
